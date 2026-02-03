629467e0852bcaa409ad4c65119f7017ab9d347d
jor-ch <chtoh97@gmail.com>
2026-02-03 22:10:39 +0800

    Initial code to make sure kivy can compile and run as intended
    Initial code to make sure kivy can compile and run as intended, using a BoxLayout with a button and label

---
+++ SOW_main.py
@@ -0,0 +1,14 @@
+import kivy
+
+kivy.require('2.3.1')
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class SOWapp(App):
+    pass
+
+
+if __name__ == '__main__':
+    SOWapp().run()
