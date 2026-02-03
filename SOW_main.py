5dd762735d3ae7c5ccbf2c1b93cf58ed014d7fa1
jor-ch <chtoh97@gmail.com>
2026-02-03 23:03:23 +0800

    Added window manager to toggle between 4 screens when app is running
    4 screens are:
    1. Welcome screen, which is the main menu
    2. Add players, where players manually key in their name and become a player
    3. Game play, where the game information is being recorded and calculated (eg. payday, purchase stock etc)
    4. Results screen, showing the winner and his results

--- SOW_main.py
+++ SOW_main.py
@@ -4,7 +4,11 @@ kivy.require('2.3.1')
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
 
+
+class WindowManager(ScreenManager):
+    pass
 
 class SOWapp(App):
     pass
