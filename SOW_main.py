1e96693065e14e93430dd97a416d3bf0e64923ea
jor-ch <chtoh97@gmail.com>
2026-02-04 23:05:27 +0800

    Added popup window to input player details in the AddPlayersScreen
    Features implemented:
    1. Popup windown that appears when "Add player" button is pressed in AddPlayersScreen
    2. Popup window allows player to input their name, starting income and starting cash
    3. Clear textinput if an invalid entry is submitted for income and cash. For now the error message is printed in the command window, but that would change to a popup message in the next few modifications
    4. Print all input parameters. In next few modifications, details would be saved in a player class for gameplay.

--- SOW_main.py
+++ SOW_main.py
@@ -1,4 +1,7 @@
 import kivy
+from kivy.factory import Factory
+from kivy.properties import StringProperty, NumericProperty
+from kivy.uix.popup import Popup
 
 kivy.require('2.3.1')
 
@@ -10,8 +13,37 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 class WindowManager(ScreenManager):
     pass
 
+class AddPlayerPopup(Popup):
+    PlayerName = StringProperty("")
+    PlayerIncome = NumericProperty(0.0)
+    PlayerCash = NumericProperty(0.0)
+
+    def SavePlayerInfo(self):
+        self.PlayerName = self.ids.name_input.text
+        print("player name is", self.PlayerName)
+        try:
+            self.PlayerIncome = float(self.ids.income_input.text)
+            print("player income is", self.PlayerIncome)
+        except ValueError:
+            print("Invalid player income! put a valid number")
+            self.ids.income_input.text = ""
+            return
+        try:
+            self.PlayerCash = float(self.ids.cash_input.text)
+            print("player cash is", self.PlayerCash)
+        except ValueError:
+            print("Invalid player cash! put a valid number")
+            return
+        self.dismiss()
+
+
+
 class SOWapp(App):
-    pass
+
+    def ShowAddPlayerPopup(self):
+        popup = Factory.AddPlayerPopup()
+        popup.open()
+
 
 
 if __name__ == '__main__':
