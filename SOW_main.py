072a550cc1f78409bb21048615923a1bb0c2b1a7
jor-ch <chtoh97@gmail.com>
2026-02-06 23:38:41 +0800

    Added PlayerName property to the PlayerButton, which would be displayed after adding a player.
    Added PlayerName property to the PlayerButton, which would be displayed after adding a player. Point is to try to get player's information using the name as a key to access the player's details via a dictionary

--- SOW_main.py
+++ SOW_main.py
@@ -1,6 +1,7 @@
 import kivy
 from kivy.factory import Factory
 from kivy.properties import StringProperty, NumericProperty
+from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 
 kivy.require('2.3.1')
@@ -38,6 +39,9 @@ class WindowManager(ScreenManager):
 class AddPlayersScreen(Screen):
     pass
 
+class PlayerButton(Button):
+    PlayerName = StringProperty("")
+
 class AddPlayerPopup(Popup):
     PlayerName = StringProperty("")
     PlayerIncome = NumericProperty(0.0)
@@ -64,11 +68,14 @@ class AddPlayerPopup(Popup):
             self.ShowInvalidEntryPopup()
             return
 
+
         app = App.get_running_app()
+        AddedPlayer = Player(self.PlayerName, self.PlayerIncome, self.PlayerCash)
         target_screen = app.root.get_screen("AddPlayers")
         player_list_container = target_screen.ids.player_list
-        player_button = Factory.PlayerButton()
+        player_button = Factory.PlayerButton(text = AddedPlayer.name, PlayerName = AddedPlayer.name)
         player_list_container.add_widget(player_button)
+        print ("name on the button is", player_button.PlayerName)
 
         self.dismiss()
 
